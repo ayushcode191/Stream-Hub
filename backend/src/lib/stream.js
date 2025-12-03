065a742b1baba2cdfff45fa51105af0b872207ff
Ayush Bansal <bansalayuayu151185@gmail.com>
2025-12-03 13:00:47 +0530

    first commit

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,26 @@
+import {StreamChat} from "stream-chat";
+import "dotenv/config";
+
+const apikey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+if(!apikey || !apiSecret){
+    console.log("Stream API  key or Secret is missing");
+}
+
+const streamClient = StreamChat.getInstance(apikey,apiSecret);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.error("Error upserting Stream user:", error);
+  }
+};
+
+
+// ToDo : do it later
+export const generateStreamToken = (userId) => {
+
+}
