04caa47da8ad32d0101704ec1465b0c403922dea
Ayush Bansal <bansalayuayu151185@gmail.com>
2025-12-05 23:39:25 +0530

    "chat section done"

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -20,7 +20,15 @@ export const upsertStreamUser = async (userData) => {
 };
 
 
-// ToDo : do it later
-export const generateStreamToken = (userId) => {
 
+export const generateStreamToken = (userId)=> {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token:", error);
+  }
+   
+  
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers,getMyFriends, sendFriendRequest } from "../controllers/user.controller.js";
+import { getRecommendedUsers,getMyFriends, sendFriendRequest, acceptFriendrequest,getFriendRequests, getOutgoingFriendReqs} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,8 +11,10 @@ router.get("/",  getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
-router.post("/friend-request/:id/accept", acceptFriendrequest);
+router.put("/friend-request/:id/accept", acceptFriendrequest);
 
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 
 export default router;
