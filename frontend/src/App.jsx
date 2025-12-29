27418098ff062094539e90d2da53278b2b8b37d6
Ayush Bansal <bansalayuayu151185@gmail.com>
2025-12-29 23:10:00 +0530

    "Video Call done!"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,18 @@ const App = () => {
             )
           }
         />
-        <Route path='/call' element={isAuthenticated ? <CallPage/> : <Navigate to="/login" />}/>
+        
+        <Route
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
+
         <Route
           path='/chat/:id'
           element={
@@ -88,6 +99,7 @@ const App = () => {
             )
           }
         />
+        
       </Routes>
 
     </div>
